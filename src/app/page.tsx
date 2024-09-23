9a1d0144c059b2259c01bb7a40b5c954ad332b58
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-09-23 13:16:54 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Questions from "@/components/Question/Questions";
+
+export default function Home() {
+
+    return (
+        <main>
+            <Questions/>
+        </main>
+    )
+}
+
---
+++ src/components/Question/Questions.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import questionsandanswers from "./data/questionsandanswers.json";
+
+import React, {useState} from 'react';
+import {Button, Heading} from "@navikt/ds-react";
+
+
+export interface Question {
+    id: number;
+    question: string;
+    answer: string;
+}
+
+
+const Questions = () => {
+
+    const [randomQuestions, setRandomQuestions] = useState<Question[]>();
+
+
+    const find10Questions = async () => {
+        const questions = find10RandomQuestions()
+        setRandomQuestions(questions);
+    }
+
+    return (
+
+        <div className="flex min-h-screen flex-col p-16 md:items-center md:p-24">
+            <Button className="mb-4" variant="primary" size="medium" onClick={find10Questions}>Random questions</Button>
+            <div className="flex min-h-screen flex-col p-16 md:p-24">
+                {randomQuestions?.map(question =>
+                    <div key={question.id} className="mt-10">
+                        <Heading size="large" level="1" spacing>{question.question} </Heading>
+                        <Heading size="medium" level="1" spacing>Svar: {question.answer} </Heading>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+function find10RandomQuestions(): Question[] {
+    const questions: Question[] = []
+
+    for (let i = 0; i < 10; i++) {
+        const question = findUniqueRandomQuestion(questions)
+        questions.push(question)
+        //console.log(`inside of logg: ${i}`)
+    }
+
+    return questions
+}
+
+function findRandomQuestion(): Question {
+    return questionsandanswers[Math.floor(Math.random() * questionsandanswers.length)]
+}
+
+function findUniqueRandomQuestion(questions: Question[]): Question {
+    let question = findRandomQuestion()
+    while (questions.find(currentQuestion => currentQuestion.id == question.id)) {
+        question = findRandomQuestion()
+    }
+    return question;
+}
+
+export default Questions;
